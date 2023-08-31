5fc773468b4e4e8c8f273b267652297d3c4b48d2
Syed Umar <khan.afrin02@gmail.com>
2023-08-31 13:13:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Employee } from './componenet/Employee';
+function App() {
+  return (
+  <div>
+  <Employee/>
+  <Employee/>
+  <Employee/>
+  <Employee/>
+    <h1>React JS Module</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis nostrum quisquam placeat minima labore? Autem dicta id in molestiae itaque, dolorum exercitationem quam, nobis facilis inventore optio cupiditate obcaecati eius suscipit ut earum consequatur repudiandae voluptates atque dolorem magni ex. Est sint error delectus ipsam voluptates facilis, culpa eaque natus nulla praesentium modi impedit. Voluptatem, hic voluptatibus neque nisi quia praesentium recusandae cum? Ex, eligendi praesentium dolorem iusto sequi molestiae magni dignissimos similique ullam. Rem doloribus reiciendis qui doloremque tempore vel autem modi voluptas, a harum exercitationem accusantium quisquam veniam odio eligendi iure dolor ea. Unde voluptates, tenetur harum quia dolore sapiente placeat, laboriosam, esse similique animi minus. Impedit quod incidunt, odit animi sapiente quaerat distinctio! Explicabo, hic suscipit ut dolorum, unde minima vero sed repellat molestiae ullam quo dolor aliquid iure similique? Est praesentium ex id odio deserunt mollitia minus odit fuga optio ipsa illum, reprehenderit fugiat obcaecati ad atque exercitationem, animi nobis asperiores! Architecto illum enim, modi consequatur ipsa esse consequuntur nemo nesciunt sunt magni odio magnam reprehenderit exercitationem, nihil, tenetur recusandae facere similique amet. Quibusdam, porro? Doloribus soluta fuga odit eum eaque eius vitae distinctio veniam autem ab, non sequi, hic minima laboriosam animi voluptatum aut, velit omnis atque modi nostrum fugit neque. Quasi consequatur pariatur dignissimos tempore aliquid est sapiente praesentium adipisci, iure velit assumenda cumque, omnis soluta, similique iste repellendus alias sunt dolore laborum eaque earum unde cum? Porro earum praesentium quis inventore laborum minima, reiciendis corporis dicta repellendus, voluptatum recusandae, suscipit commodi. Assumenda, id.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias tenetur obcaecati ea commodi at dignissimos fugiat ipsa, quisquam dolore hic atque laboriosam non maiores porro laborum labore, soluta odit voluptatem doloribus iste voluptas neque. Sed vero quisquam asperiores soluta provident odio, magnam ad velit itaque tempore nisi exercitationem quo facere ea fuga ut qui sequi, nihil minus veritatis. Ipsa quo magni exercitationem ut consequatur laudantium! Nam, natus! Voluptas porro, esse eius voluptate fugiat autem reiciendis, odit fuga commodi facilis explicabo corrupti doloribus adipisci quasi soluta sequi consectetur consequatur velit, tempora saepe veritatis officiis deleniti. Culpa eveniet atque cupiditate sequi autem?</p>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima laboriosam libero quos inventore ipsa sequi illum ut aliquam, rerum soluta repellat, nemo nesciunt dolorem dolore, quaerat voluptas facilis tempore voluptatem!</p>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/componenet/Employee.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export const Employee=()=>{
+    return(
+        <div>
+            <h3>Node</h3>
+            <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore sunt provident voluptate voluptatum, aperiam nobis corrupti praesentium neque exercitationem fugit, at a molestias quibusdam voluptates suscipit quod quis ratione tempora.
+                </p>
+        </div>
+    )
+}
